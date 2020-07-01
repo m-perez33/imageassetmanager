79c3d71d1b32fb9c44f1f6fc2accb6a400d2f02f
m-perez33 <kingleisure@gmail.com>
2020-07-01 11:46:52 -0400

    Updates

---
+++ precache-manifest.43d8fba6c571b18a3664609a8612bd09.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09df80f58f799208a99faf9b8442ed34",
+    "url": "/imageassetmanager/index.html"
+  },
+  {
+    "revision": "775fd394c0039e4d7185",
+    "url": "/imageassetmanager/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "30c2cf2e56c562a6ec46",
+    "url": "/imageassetmanager/static/css/main.06846b7f.chunk.css"
+  },
+  {
+    "revision": "775fd394c0039e4d7185",
+    "url": "/imageassetmanager/static/js/2.1337745a.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/imageassetmanager/static/js/2.1337745a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c2cf2e56c562a6ec46",
+    "url": "/imageassetmanager/static/js/main.e63daaf5.chunk.js"
+  },
+  {
+    "revision": "88f62c0528c1343fe071",
+    "url": "/imageassetmanager/static/js/runtime-main.ef7ace83.js"
+  },
+  {
+    "revision": "9c9fa3b93a62a688a4023f85a930ae49",
+    "url": "/imageassetmanager/static/media/Grandma.9c9fa3b9.jpg"
+  },
+  {
+    "revision": "fd1e408a210181ed1f4991ab0e544a66",
+    "url": "/imageassetmanager/static/media/IMG_8912.fd1e408a.jpg"
+  },
+  {
+    "revision": "dd818dfed4037b9771a07c4feb3d992b",
+    "url": "/imageassetmanager/static/media/atkins.dd818dfe.jpg"
+  },
+  {
+    "revision": "a18fa463471709c711548e5e2d63eeb4",
+    "url": "/imageassetmanager/static/media/costello.a18fa463.jpg"
+  },
+  {
+    "revision": "cfe0abc8b4c55e3ad39c2b5d207046ef",
+    "url": "/imageassetmanager/static/media/cousins.cfe0abc8.jpg"
+  },
+  {
+    "revision": "53e95a93d709c2bff0dd93c1ac1bb833",
+    "url": "/imageassetmanager/static/media/emgabe.53e95a93.jpg"
+  },
+  {
+    "revision": "36cb675b84cd42a94c71aaf9ef8905d4",
+    "url": "/imageassetmanager/static/media/franklin.36cb675b.jpg"
+  },
+  {
+    "revision": "72d1fa46d9e608c6195ba8cf0ae850a9",
+    "url": "/imageassetmanager/static/media/great.72d1fa46.jpg"
+  },
+  {
+    "revision": "893dbc1b9430862c3b5b5669f55a1fbe",
+    "url": "/imageassetmanager/static/media/jimi.893dbc1b.jpg"
+  },
+  {
+    "revision": "9178ad071d8ad25b5cf1e99cacebb69a",
+    "url": "/imageassetmanager/static/media/laura.9178ad07.jpg"
+  },
+  {
+    "revision": "04f977f63f3079334b958c5efe42d7d6",
+    "url": "/imageassetmanager/static/media/model.04f977f6.jpg"
+  },
+  {
+    "revision": "26e485ef010c965d39cf9dfee467d0db",
+    "url": "/imageassetmanager/static/media/price.26e485ef.jpg"
+  },
+  {
+    "revision": "2ff60969cc2c6a108814030167a5c8e1",
+    "url": "/imageassetmanager/static/media/segarra.2ff60969.jpg"
+  },
+  {
+    "revision": "5925ae24654f8d5caffc9dd0129df14f",
+    "url": "/imageassetmanager/static/media/turner.5925ae24.jpg"
+  },
+  {
+    "revision": "823098dcd5f646405f978cb464f06710",
+    "url": "/imageassetmanager/static/media/unnamed.823098dc.jpg"
+  }
+]);
